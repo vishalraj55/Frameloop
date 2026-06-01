24114fa7eac583439b4b50bf9a8c92df996144f6
Vishal <gamingdark340@gmail.com>
2026-06-01 12:57:35 +0530

    fix app controller registration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
@@ -19,5 +21,7 @@ import { FirebaseModule } from './firebase/firebase.module';
     CommentsModule,
     NotificationsModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
